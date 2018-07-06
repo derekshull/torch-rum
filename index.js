21cd5fc85906aa6105a1b4adcbf5a688a1754f89
Matt Shull <derekshull@gmail.com>
2018-07-06 17:20:41 -0500

    change port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'torch';
 
 const app = express();
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));// parse application/x-www-form-urlencoded
 app.use(bodyParser.json());// parse application/json
