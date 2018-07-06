f282f7ed25270fb20ec26d6ce5847391d6690a55
Matt Shull <derekshull@gmail.com>
2018-07-06 17:33:22 -0500

    change route

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));// parse application/x-www-f
 app.use(bodyParser.json());// parse application/json
 app.use(cookieParser());// parse request cookies
 
-app.post('/store-beacon', (req, res) => {
+app.post('/api/store-beacon', (req, res) => {
   try {
     const bodyPid = req.body.pid;
 
